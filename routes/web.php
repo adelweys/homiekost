2335ba0c70189bffa55e648c2fd8a6d43d1325be
MuhammadAzisSaputra <azizsyahputra0311@gmail.com>
2023-05-30 18:14:50 +0700

    front-end
    Update ftont-end, cuman belum di layouting jadi masij basic kali

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,63 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/daftarKamar', function () {
+    return view('daftarKamar');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/daftarKos', function () {
+    return view('daftarKos');
+});
+
+Route::get('/detailKos', function () {
+    return view('detailKos');
+});
+Route::get('/form', function () {
+    return view('form');
+});
+
+Route::get('/forms', function () {
+    return view('forms');
+});
+
+Route::get('/kos_card', function () {
+    return view('kos_card');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/sewa', function () {
+    return view('sewa');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
 });
