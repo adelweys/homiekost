ea318b4b42a204b8cf5664eb9949ad9c2696c082
Steven Valentino <huangwieyang@gmail.com>
2023-06-09 17:06:00 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -117,6 +117,166 @@ class CostSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Azis',
+            'total_kamar' => '5',
+            'cost_type' => 'pria',
+            'available_room' => '2',
+            'photo' => '',
+            'cost_location' => 'Iskandar muda',
+            'cost_address' => 'Jalan Iskandar Muda No. 24C',
+            'description' => 'Kos Pak Azis yang berlokasi di Iskandar muda dengan kamar seluas 12 meter persegi atau 4 x 3 meter.',
+            'contact_person' => '081223146587',
+            'night_limit' => '2',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Ariyo',
+            'total_kamar' => '9',
+            'cost_type' => 'wanita',
+            'available_room' => '5',
+            'photo' => '',
+            'cost_location' => 'Dr. Mansur',
+            'cost_address' => 'Jalan Dr. Mansur No. 65B',
+            'description' => 'Kos Pak Azis yang berlokasi di Iskandar muda dengan kamar seluas 12 meter persegi atau 4 x 3 meter.',
+            'contact_person' => '083265458195',
+            'night_limit' => '2',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Susan',
+            'total_kamar' => '9',
+            'cost_type' => 'wanita',
+            'available_room' => '5',
+            'photo' => '',
+            'cost_location' => 'Dr. Mansur',
+            'cost_address' => 'Jalan Dr. Mansur No. 62E',
+            'description' => 'Kos Bu Susan yang berlokasi di Dr. Mansur dengan kamar seluas 16 meter persegi atau 4 x 4 meter.',
+            'contact_person' => '083265458195',
+            'night_limit' => '2',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Budi',
+            'total_kamar' => '5',
+            'cost_type' => 'wanita',
+            'available_room' => '4',
+            'photo' => '',
+            'cost_location' => 'Dr. Mansur',
+            'cost_address' => 'Jalan Dr. Mansur No. 1A',
+            'description' => 'Kos Pak Budi yang berlokasi di Dr. Mansur dengan kamar seluas 20 meter persegi atau 5 x 4 meter.',
+            'contact_person' => '084658973121',
+            'night_limit' => '1',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Santi',
+            'total_kamar' => '6',
+            'cost_type' => 'campuran',
+            'available_room' => '3',
+            'photo' => '',
+            'cost_location' => 'Setia Budi',
+            'cost_address' => 'Jalan Setia Budi No. 4C',
+            'description' => 'Kos Bu Santi yang berlokasi di Dr. Mansur dengan kamar seluas 20 meter persegi atau 5 x 4 meter.',
+            'contact_person' => '082345646545',
+            'night_limit' => '4',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '2',
+            'cost_name' => 'Kos Sandi',
+            'total_kamar' => '10',
+            'cost_type' => 'pria',
+            'available_room' => '1',
+            'photo' => '',
+            'cost_location' => 'Iskandar Muda',
+            'cost_address' => 'Jalan Iskandar muda No. 8E',
+            'description' => 'Kos Pak Sandi yang berlokasi di Iskandar Muda dengan kamar seluas 12 meter persegi atau 3 x 4 meter.',
+            'contact_person' => '082346531291',
+            'night_limit' => '1',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '5',
+            'cost_name' => 'Kos Rossy',
+            'total_kamar' => '8',
+            'cost_type' => 'wanita',
+            'available_room' => '3',
+            'photo' => '',
+            'cost_location' => 'Iskandar Muda',
+            'cost_address' => 'Jalan Iskandar muda No. 108U',
+            'description' => 'Kos Bu Rossy yang berlokasi di Iskandar Muda dengan kamar seluas 12 meter persegi atau 3 x 4 meter.',
+            'contact_person' => '081398624299',
+            'night_limit' => '1',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '5',
+            'cost_name' => 'Kos Harry',
+            'total_kamar' => '4',
+            'cost_type' => 'pria',
+            'available_room' => '1',
+            'photo' => '',
+            'cost_location' => 'Setia Budi',
+            'cost_address' => 'Jalan Setia Budi No. 12A',
+            'description' => 'Kos Pak Harry yang berlokasi di Setia Budi dengan kamar seluas 9 meter persegi atau 3 x 3 meter.',
+            'contact_person' => '083149134751',
+            'night_limit' => '1',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '5',
+            'cost_name' => 'Kos Sani',
+            'total_kamar' => '6',
+            'cost_type' => 'pria',
+            'available_room' => '2',
+            'photo' => '',
+            'cost_location' => 'Iskandar Muda',
+            'cost_address' => 'Jalan Iskandar Muda No. 21C',
+            'description' => 'Kos Bu Sani yang berlokasi di Iskandar Muda dengan kamar seluas 9 meter persegi atau 3 x 3 meter.',
+            'contact_person' => '083149134751',
+            'night_limit' => '3',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
+        DB::table('costs')->insert([
+            'user_id' => '5',
+            'cost_name' => 'Kos Kelly',
+            'total_kamar' => '7',
+            'cost_type' => 'campuran',
+            'available_room' => '2',
+            'photo' => '',
+            'cost_location' => 'Setia Budi',
+            'cost_address' => 'Jalan Setia Budi No. 40B',
+            'description' => 'Kos Bu Kelly yang berlokasi di Iskandar Muda dengan kamar seluas 9 meter persegi atau 3 x 3 meter.',
+            'contact_person' => '083149134751',
+            'night_limit' => '1',
+            'long_add' => '',
+            'lat_add' => '',
+        ]);
+
         $faker = Faker::create();
 
         $ownerIds = User::whereIn('id', [2, 5])->pluck('id')->toArray();
@@ -138,7 +298,7 @@ class CostSeeder extends Seeder
                 'cost_type' => $faker->randomElement(['pria', 'wanita', 'campuran']),
                 'available_room' => $faker->numberBetween(0, 10),
                 'photo' => $faker->imageUrl(),
-                'cost_location' => $faker->randomElement(['Dr. Mansur', 'Jamin Ginting', 'Setia Budi', 'Iskandar muda']),
+                'cost_location' => $faker->randomElement(['Dr. Mansur', 'Jamin Ginting', 'Setia Budi', 'Iskandar Muda']),
                 'cost_address' => $faker->address,
                 'description' => $faker->paragraph,
                 'contact_person' => $faker->unique(true)->numberBetween(100000000000, 999999999999),
