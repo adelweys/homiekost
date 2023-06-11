c9a8a7ce6cd72f9779237a60f98648e1435e7503
muhammadaryaananda <arya.oppo95@gmail.com>
2023-06-11 22:15:57 +0700

    icon bro

--- resources/views/main/detail-kos.blade.php
+++ resources/views/main/detail-kos.blade.php
@@ -218,13 +218,14 @@
 
                                 {{-- closet --}}
                                 @if ($room->closet == 'duduk')
-                                
-                                    <h6><i><img src="{{asset('/img/tduduk.svg')}}" style="padding: 0%;" />
-                                    </i>Kloset Duduk</h6>
+                               
+                                    <h6>
+                                        <i><svg fill="#000000" height="18" width="18" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 511.998 511.998" xml:space="preserve" data-darkreader-inline-fill="" style="--darkreader-inline-fill: #181a1b;"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M299.102,0v42.278v30.417V225.11H20.279v42.278v25.349v5.068h0.088c0.89,28.175,9.183,55.51,24.136,79.35 c13.72,21.875,32.505,40.054,54.693,53.018v81.824h301.282v-80.794c0-19.06,6.787-37.564,19.113-52.103l45.798-54.024 c16.979-20.028,26.329-45.516,26.329-71.771V72.695V42.278V0H299.102z M50.696,255.529h248.406v11.861H50.696V255.529z M461.303,253.307c0,19.059-6.787,37.563-19.113,52.102l-45.798,54.023c-16.979,20.027-26.329,45.515-26.329,71.772v50.378 H129.615V411.81l-8.331-4.224c-42.024-21.305-68.685-63.048-70.489-109.78h278.725v-72.695V72.695h131.783V253.307z M461.303,42.278H329.519V30.417h131.783V42.278z"></path> </g> </g> </g></svg></i>
+                                    Kloset Duduk</h6>
                                 
                                 @elseif($room->closet == 'jongkok')
 
-                                        <h6><i><img src="{{asset('/img/tjongkok.svg')}}" style="padding: 0%;" />
+                                        <h6><i><svg fill="#000000" height="18" width="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill="" style="--darkreader-inline-fill: #181a1b;"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M20.062,4.25l-3.4-3A1,1,0,0,0,16,1H8a1,1,0,0,0-.662.25l-3.4,3a1,1,0,0,0-.329.618l-1.6,12A1,1,0,0,0,3,18H5.111C5.756,20.883,9.09,23,12,23s6.244-2.117,6.889-5H21a1,1,0,0,0,.991-1.132l-1.6-12A1,1,0,0,0,20.062,4.25ZM18,16a1,1,0,0,0-1,1c0,2.154-2.75,4-5,4s-5-1.846-5-4a1,1,0,0,0-1-1H4.142l.133-1H6a1,1,0,0,0,0-2H4.542l.134-1H6a1,1,0,0,0,0-2H4.942l.134-1H6A1,1,0,0,0,6,7H5.342l.2-1.5L8.378,3h7.244l2.836,2.5.2,1.5H18a1,1,0,0,0,0,2h.924l.134,1H18a1,1,0,0,0,0,2h1.324l.134,1H18a1,1,0,0,0,0,2h1.725l.133,1ZM13,5H11A3,3,0,0,0,8,8v6a4,4,0,0,0,8,0V8A3,3,0,0,0,13,5Zm1,9a2,2,0,0,1-4,0V8a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1Z"></path></g></svg>
                                         </i>Kloset jongkok</h6>
                               
                                 @else
